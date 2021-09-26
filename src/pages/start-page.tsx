77055ab12af4878b2e0d34edfea281e8c34c3ef8
xareyli <xareyli@gmail.com>
2021-09-26 17:17:31 +0300

    Added layout of start page

--- src/pages/start-page.tsx
+++ src/pages/start-page.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
+import Group from '@material-ui/icons/Group';
+import styles from './start-page.module.scss';
 
 const StartPage: FC = () => {
     return (
         <div>
-            Панель администратора
+            <h1 className={styles.header}>Панель администратора</h1>
+
+            <div className="container">
+                <div className={styles.entitiesList}>
+                    <span className={styles.entitiesList__item}>
+                        <Group classes={{root: styles.entitiesList__icon}} />
+                        Пользователи
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
